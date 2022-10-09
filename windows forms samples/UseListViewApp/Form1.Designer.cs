9befd4f22cffcf4062cfe96c2ed348c9dc23e90c
kenleycl07 <78169956+kenleycl07@users.noreply.github.com>
2022-10-09 16:24:07 -0700

    Add files via upload

---
+++ windows forms samples/UseListViewApp/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace UsingListView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.listView = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.listViewFile = new System.Windows.Forms.ListView();
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID:";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(93, 93);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(93, 41);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(132, 20);
+            this.txtID.TabIndex = 2;
+            // 
+            // listView
+            // 
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView.HideSelection = false;
+            this.listView.Location = new System.Drawing.Point(52, 157);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(301, 192);
+            this.listView.TabIndex = 3;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ID";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Name";
+            this.columnHeader2.Width = 150;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(93, 67);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(132, 20);
+            this.txtName.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Name:";
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(174, 93);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 6;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(916, 355);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenFile.TabIndex = 7;
+            this.btnOpenFile.Text = "Open";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // listViewFile
+            // 
+            this.listViewFile.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listViewFile.HideSelection = false;
+            this.listViewFile.Location = new System.Drawing.Point(411, 157);
+            this.listViewFile.Name = "listViewFile";
+            this.listViewFile.Size = new System.Drawing.Size(580, 192);
+            this.listViewFile.TabIndex = 8;
+            this.listViewFile.UseCompatibleStateImageBehavior = false;
+            this.listViewFile.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "File Name";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "File Path";
+            this.columnHeader4.Width = 150;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1116, 456);
+            this.Controls.Add(this.listViewFile);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.ListView listViewFile;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+    }
+}
+
---
+++ windows forms samples/UseListViewApp/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UsingListView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtID.Text) || string.IsNullOrEmpty(txtName.Text))
+                return;
+            ListViewItem item = new ListViewItem(txtID.Text);
+            item.SubItems.Add(txtName.Text);
+            listView.Items.Add(item);
+            txtID.Clear();
+            txtName.Clear();
+            txtID.Focus();
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (listView.Items.Count > 0) 
+            {
+                listView.Items.Remove(listView.SelectedItems[0]);
+            }
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "All files|*.*",
+                ValidateNames = true, Multiselect = true})
+            {
+                if (ofd.ShowDialog() == DialogResult.OK) 
+                {
+                    foreach (string f in ofd.FileNames) 
+                    { 
+                        FileInfo fi = new FileInfo(f);
+                        ListViewItem item = new ListViewItem(fi.Name);
+                        item.SubItems.Add(fi.FullName);
+                        listViewFile.Items.Add(item);
+                    }
+                }
+            }
+           
+        }
+    }
+}
