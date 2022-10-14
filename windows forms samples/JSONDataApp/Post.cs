4f3ff232b20e62b69b66336abf0203861475de19
kenleycl07 <78169956+kenleycl07@users.noreply.github.com>
2022-10-14 19:27:37 -0400

    Add files via upload

---
+++ windows forms samples/JSONDataApp/Post.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONDataApp
+{
+        // Root myDeserializedClass = JsonConvert.DeserializeObject<List<Root>>(myJsonResponse);
+        public class Post
+        {
+            [JsonProperty("id")]
+            public int Id { get; set; }
+
+            [JsonProperty("title")]
+            public string Title { get; set; }
+        }
+    
+}
---
+++ windows forms samples/JSONDataApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+//  use this link to transform json content into objects (classes): https://json2csharp.com/
+namespace JSONDataApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string url = "https://my-json-server.typicode.com/typicode/demo/posts";
+            // http client to send the get request
+            HttpClient httpClient = new HttpClient();
+
+            try
+            {
+                var httpResponseMessage = await httpClient.GetAsync(url);
+
+                // read the string from the response content
+                string jsonResponse = await httpResponseMessage.Content.ReadAsStringAsync();
+                // print jsonResponse
+                Console.WriteLine(jsonResponse);
+
+                // Deserialize the json response into a C# array of type Post[]
+                var myPosts = JsonConvert.DeserializeObject<Post[]>(jsonResponse);
+
+                // print the array objects using a for each loop
+                foreach (var post in myPosts)
+                {
+                    // print the id and the title of each post
+                    Console.WriteLine($"{post.Id} {post.Title}");
+                }
+                Console.Read();
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                // Dispose of the httpClient
+                httpClient.Dispose();
+            }
+
+        }
+    }
+}
