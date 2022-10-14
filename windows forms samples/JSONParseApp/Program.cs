a09a001f9b6f01ed269701c1c8e05f0250248907
kenleycl07 <78169956+kenleycl07@users.noreply.github.com>
2022-10-14 19:48:52 -0400

    Add files via upload

---
+++ windows forms samples/JSONParseApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONParseApp
+{
+    class Program
+    {
+        HttpClient client = new HttpClient();
+
+        static async Task Main(string[] args)
+        {
+            Program program = new Program();
+            await program.GetTodoItems();
+        }
+
+        private async Task GetTodoItems()
+        {
+            string response = await client.GetStringAsync("https://my-json-server.typicode.com/typicode/demo/posts");
+            
+            Console.WriteLine(response);
+
+            List<Todo> todo = JsonConvert.DeserializeObject<List<Todo>>(response);
+            foreach (var item in todo) 
+            {
+                Console.WriteLine(item.Id);
+                Console.WriteLine(item.Title);
+            }
+
+            Console.Read();
+        }
+
+        class Todo 
+        { 
+            public int Id { get; set; } 
+            public string Title { get; set; }
+        }
+    }
+}
